651ef1d4869dc787c75810a83958ca9e6fef9bb0
Alexander Walker <alexwalker1@live.co.uk>
2018-06-07 01:54:11 +0400

    ATC-001 :lipstick: adding styled components package

--- src/components/Products/components/Products.jsx
+++ src/components/Products/components/Products.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import MediaQuery from 'react-responsive'
+import styled from 'styled-components'
 
 import ProductItem from './ProductItem'
 
